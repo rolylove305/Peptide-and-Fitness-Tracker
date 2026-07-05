b1665b126c588693e75da12cdb9b1db447896798
rolylove305 <rolylove305@gmail.com>
2026-07-05 12:22:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL='https://gfuazkuohrragkthtumz.supabase.co';
-window.SUPABASE_ANON_KEY='sb_publishable_HJNw6TpNJ0YwTo4rEME6zg_Gbp1xtGU';
+window.SUPABASE_ANON_KEY="sb_publishable_HJNw6TpNJ0YwTo4rEME6zg_Gbp1xtGU";
